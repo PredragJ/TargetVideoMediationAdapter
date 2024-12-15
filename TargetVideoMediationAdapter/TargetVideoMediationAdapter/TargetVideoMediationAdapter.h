0902c9e634628edc0dbff5d8b1043696a74300f1
Predrag Jevtic <predragjevtic@gmail.com>
2024-12-15 12:56:28 +0100

    Initial Commit

---
+++ TargetVideoMediationAdapter/TargetVideoMediationAdapter/TargetVideoMediationAdapter.h
@@ -0,0 +1,18 @@
+//
+//  TargetVideoMediationAdapter.h
+//  TargetVideoMediationAdapter
+//
+//  Created by Predrag Jevtic on 15.12.24..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TargetVideoMediationAdapter.
+FOUNDATION_EXPORT double TargetVideoMediationAdapterVersionNumber;
+
+//! Project version string for TargetVideoMediationAdapter.
+FOUNDATION_EXPORT const unsigned char TargetVideoMediationAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TargetVideoMediationAdapter/PublicHeader.h>
+
+
